4ba19331542d70d59873ad2cf5bc417046e3b58f
她的名字？H <H@LAPTOP-KN2HO20J>
2018-08-28 17:26:33 +0800

    测试

---
+++ src/main/java/com/yihao86/pojo/Course.java
@@ -0,0 +1,15 @@
+package com.yihao86.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Course {
+
+	private int crid;
+	
+	private int crname;
+	
+	private int cr_aid;
+}
