eb14633a689d6403d518e3a0b2dd2a75665168c6
她的名字？H <H@LAPTOP-KN2HO20J>
2018-09-01 11:52:02 +0800

    222

---
+++ src/main/java/com/yihao86/service/impl/TypeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.yihao86.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yihao86.dao.TypeDao;
+import com.yihao86.dao.VideosDao;
+import com.yihao86.pojo.Type;
+import com.yihao86.service.TypeService;
+
+@Service
+@Transactional
+@AutoConfigureAfter({TypeDao.class})
+public class TypeServiceImpl implements TypeService {
+
+	
+	@Autowired
+	private TypeDao tdao;
+	
+	@Override
+	public List<Type> selectType() {
+		// TODO Auto-generated method stub
+		return tdao.findType();
+	}
+
+	@Override
+	public List<Type> selectSonList(int f_trid) {
+		
+		
+		
+		return tdao.findSonList(f_trid);
+	}
+
+
+
+	
+
+}
